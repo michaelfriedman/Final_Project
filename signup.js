0592d1e885b8cfc8fdadd2aa6f5ded4af48729c3
Michael Friedman <contact@michaeldavidfriedman.com>
2016-07-08 15:06:07 -0700

    Currently working on JavaScript to incorporate our checkboxes into the JSON object.

--- signup.js
+++ signup.js
@@ -1,16 +1,27 @@
 var userProfile = {
   'users': []
-};
+}
 
 function User(name, email) {
   this.name = name;
   this.email = email;
+  this.netflix;
+  this.hulu;
+  this.hboGo;
 }
 
 document.getElementById('submit').onclick = function() {
   var name = document.getElementById('username').value;
   var email = document.getElementById('email').value;
-  var newUser = new User(name, email);
+  var netflix = document.getElementById('netflix');
+  if (document.getElementById('netflix').checked) {
+    netflix = 'yes';
+  } else {
+    netflix = 'no';
+  }
+  var hulu = document.getElementById('hulu');
+  var hboGo = document.getElementById('hboGo');
+  var newUser = new User(name, email, netflix, hulu, hboGo);
   var JsonUser = JSON.stringify(newUser);
   userProfile.users.push(JsonUser);
   localStorage.setItem('userProfile', JSON.stringify(userProfile));
