602524c479e8c30f438ceb56397811a3f6e7db61
Michael Friedman <contact@michaeldavidfriedman.com>
2016-07-08 15:43:50 -0700

    Starting to write script for checkbox eventhandling/validation.

--- signup.js
+++ signup.js
@@ -1,24 +1,16 @@
 var userProfile = {
   'users': []
-}
+};
 
 function User(name, email) {
   this.name = name;
   this.email = email;
-  this.netflix;
-  this.hulu;
-  this.hboGo;
 }
 
 document.getElementById('submit').onclick = function() {
   var name = document.getElementById('username').value;
   var email = document.getElementById('email').value;
   var netflix = document.getElementById('netflix');
-  if (document.getElementById('netflix').checked) {
-    netflix = 'yes';
-  } else {
-    netflix = 'no';
-  }
   var hulu = document.getElementById('hulu');
   var hboGo = document.getElementById('hboGo');
   var newUser = new User(name, email, netflix, hulu, hboGo);
