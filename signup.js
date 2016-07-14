01b350a95ac363b917a97d658fb5a0fe20f8224e
Michael Friedman <contact@michaeldavidfriedman.com>
2016-07-14 16:02:46 -0700

    Trying to style these pages. CSS situation is redonkulous.

--- signup.js
+++ signup.js
@@ -1,6 +1,16 @@
-var userProfile = {
-  'users': []
-};
+var userMovies = [];
+var userMovieNames = [];
+var userTempMovies = [];
+var userTempMovieNames = [];
+var tempMovieId = [];
+var sceneItMovies = [];
+var sceneItMovieNames = [];
+var sceneItMovieIds = [];
+var tempSceneItRemove = [];
+var tempCheckboxIds = [];
+var checkBoxIds = [];
+var tempSceneItMovies = [];
+var tempSceneItMovieNames = [];
 
 function User(name, email) {
   this.name = name;
@@ -8,6 +18,8 @@ function User(name, email) {
   this.netflix = netflix.checked;
   this.hulu = hulu.checked;
   this.hboNow = hboNow.checked;
+  this.watchList = [];
+  this.seenItList = [];
 }
 
 function getTheThings(event) {
@@ -15,11 +27,219 @@ function getTheThings(event) {
   var user_name = event.target.user_name.value;
   var user_email = event.target.user_email.value;
   var newUser = new User(user_name, user_email);
-  console.log(newUser);
   var JsonUser = JSON.stringify(newUser);
-  userProfile.users.push(JsonUser);
-  localStorage.setItem('userProfile', JSON.stringify(userProfile));
+  localStorage.setItem('userProfile', JsonUser);
+  var localStuff = JSON.parse(localStorage.getItem('userProfile'));
+  localStuff.shows = [];
+  localStuff.showNames = [];
+  localStuff.sceneItMovies = [];
+  localStuff.sceneItMovieNames = [];
+  localStuff.sceneItMovieIds = [];
+  localStuff.checkBoxIds = [];
+  localStorage.setItem('userProfile', JSON.stringify(localStuff));
+  var signup = document.getElementById('signup');
+  var profile = document.getElementById('profileFrontPage');
+  signup.style.display = 'none';
+  profile.style.display = 'block';
+  getUserMovies();
 };
 
 var signupForm = document.getElementById('signupForm');
 signupForm.addEventListener('submit', getTheThings);
+
+
+
+function getUserMovies() {
+  var userProfile = JSON.parse(localStorage.getItem('userProfile'));
+  sceneItMovies = userProfile.sceneItMovies;
+  sceneItMovieNames = userProfile.sceneItMovieNames;
+  sceneItMovieIds = userProfile.sceneItMovieIds;
+  tempCheckboxIds = userProfile.checkBoxIds;
+  for (var show in userProfile.shows) {
+    userMovies.push(userProfile.shows[show]);
+    userMovieNames.push(userProfile.showNames[show]);
+  }
+};
+
+function updateLocalStorage() {
+  var userProfile = JSON.parse(localStorage.getItem('userProfile'));
+  userProfile.shows = userMovies;
+  userProfile.showNames = userMovieNames;
+  userProfile.sceneItMovies = sceneItMovies;
+  userProfile.sceneItMovieNames = sceneItMovieNames;
+  userProfile.sceneItMovieIds = sceneItMovieIds;
+  userProfile.checkBoxIds = checkBoxIds;
+  localStorage.setItem('userProfile', JSON.stringify(userProfile));
+  localStorage.setItem('userProfile', JSON.stringify(userProfile));
+}
+
+function createLabels() {
+  var div = document.getElementById('watchList');
+  for (var movieName in userMovieNames) {
+    var servicesArray = userMovies[movieName].servicesArray;
+    var label = document.createElement('label');
+    label.id = userMovieNames[movieName];
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = movieName + '';
+    checkbox.style.marginLeft = '10px';
+    label.textContent = userMovieNames[movieName];
+    checkForIcons(servicesArray, label);
+    label.appendChild(checkbox);
+    div.appendChild(label);
+  }
+};
+
+function createSceneItLabels() {
+  updateLocalStorage();
+  var div = document.getElementById('sceneIt');
+  for (var scene in sceneItMovies) {
+    var servicesArray2 = sceneItMovies[scene].servicesArray;
+    var label = document.createElement('label');
+    label.id = sceneItMovieNames[scene];
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = scene + '';
+    checkbox.style.marginLeft = '10px';
+    label.textContent = sceneItMovieNames[scene];
+    checkForIcons(servicesArray2, label);
+    label.appendChild(checkbox);
+    div.appendChild(label);
+  }
+}
+
+function checkForIcons(servicesArray, label) {
+  for (var service in servicesArray) {
+    var img = document.createElement('img');
+    if (servicesArray[service] === 'HBONow') {
+      img.src = 'icons/hbonow.png';
+    } else if (servicesArray[service] === 'Hulu') {
+      img.src = 'icons/hulu.jpg';
+    } else {
+      img.src = 'icons/netflix.jpg';
+    }
+    label.appendChild(img);
+  }
+}
+
+function deleteSelectedItems(event) {
+  event.preventDefault();
+  for (var movieQty in userMovies) {
+    var truthy = document.getElementById(movieQty + '');
+    if (truthy.checked) {
+      var label = document.getElementById(userMovieNames[movieQty]);
+      label.remove();
+    } else {
+      userTempMovies.push(userMovies[movieQty]);
+      userTempMovieNames.push(userMovieNames[movieQty]);
+      tempMovieId.push(movieQty);
+    }
+  }
+  userMovies = userTempMovies;
+  userMovieNames = userTempMovieNames;
+  reassignIds();
+  updateLocalStorage();
+  // location.reload();
+};
+
+function deleteSceneItMovie() {
+  console.log('clicked');
+  for (var i = 0; i < sceneItMovieIds.length; i++) {
+    console.log('Yes');
+    console.log(tempCheckboxIds);
+    var tempElem = document.getElementById(sceneItMovieIds[i]);
+    var tempCheck = document.getElementById(tempCheckboxIds[i]);
+    console.log(tempCheck);
+    console.log(tempElem);
+    if (tempCheck.checked) {
+      tempElem.remove();
+    } else {
+      tempSceneItRemove.push(sceneItMovieIds[i]);
+      tempCheckboxIds.push(i + '');
+      tempSceneItMovies.push(sceneItMovies[i]);
+      tempSceneItMovieNames.push(sceneItMovieNames[i]);
+    }
+  }
+  sceneItMovieIds = tempSceneItRemove;
+  checkBoxIds = tempCheckboxIds;
+  tempCheckboxIds = [];
+  sceneItMovies = tempSceneItMovies;
+  sceneItMovieNames = tempSceneItMovieNames;
+  tempSceneItMovies = [];
+  tempSceneItMovieNames = [];
+  updateLocalStorage();
+  location.reload();
+};
+
+function reassignIds() {
+  if (userMovies.length) {
+    for (var id in tempMovieId) {
+      var tempElem = document.getElementById(tempMovieId[id] + '');
+      tempElem.id = id + '';
+    }
+  }
+  userTempMovies = [];
+  userTempMovieNames = [];
+  tempMovieId = [];
+}
+
+function moveToSceneIt() {
+  var sceneItDiv = document.getElementById('sceneIt');
+  for (var i = 0; i < userMovieNames.length; i++) {
+    var check = document.getElementById(i + '');
+    if (check.checked) {
+      var moveThis = document.getElementById(userMovieNames[i]);
+      var cln = moveThis.cloneNode(true);
+      sceneItDiv.appendChild(cln);
+      moveThis.remove();
+      sceneItMovies.push(userMovies[i]);
+      sceneItMovieNames.push(userMovieNames[i]);
+      sceneItMovieIds.push(userMovieNames[i]);
+      cln.id = userMovieNames[i];
+      console.log(sceneItMovieIds);
+      console.log(cln.id);
+      tempCheckboxIds.push(i + '');
+      console.log(tempCheckboxIds);
+    }
+    else {
+      userTempMovies.push(userMovies[i]);
+      userTempMovieNames.push(userMovieNames[i]);
+      tempMovieId.push(i + '');
+    }
+  }
+  userMovies = userTempMovies;
+  userMovieNames = userTempMovieNames;
+  reassignIds();
+  updateLocalStorage();
+  console.log(tempCheckboxIds);
+  // location.reload();
+};
+
+var watchListForm = document.getElementById('watchListSubmit');
+watchListForm.addEventListener('submit', deleteSelectedItems);
+var sceneItButton = document.getElementById('sceneItButton');
+sceneItButton.addEventListener('click', moveToSceneIt);
+var deleteIt = document.getElementById('deleteSceneIt');
+deleteIt.addEventListener('click', deleteSceneItMovie);
+
+(function() {
+  var signup = document.getElementById('signup');
+  var profile = document.getElementById('profileFrontPage');
+  if(localStorage.userProfile) {
+    signup.style.display = 'none';
+    profile.style.display = 'block';
+    getUserMovies();
+    createLabels();
+    createSceneItLabels();
+    var deleteButton = document.getElementById('deleteButton');
+    var sceneItButton = document.getElementById('sceneItButton');
+    var deleteSceneItButton = document.getElementById('deleteSceneIt');
+    if (userMovieNames.length > 0) {
+      deleteButton.style.display = 'block';
+      sceneItButton.style.display = 'block';
+    }
+    if (sceneItMovies.length > 0) {
+      deleteSceneItButton.style.display = 'block';
+    }
+  }
+})();
